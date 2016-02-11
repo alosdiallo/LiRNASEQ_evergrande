d0fbc4921b7aff5f15761408dc7e5de03950ffc4
ashleysn8 <ashleysn8@gmail.com>
2016-02-11 00:19:07 -0500

    removed mouse/human scripts and renamed to general scripts

--- liRNAseq_newdpzpipeline/runScript_mouse_bpf.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#This is just the /groups/bpf-evergrande/liRNAseq_scripts/lirRNAseq_pipeline.sh  script with the directory change commented out
-#cd /groups/bpf-evergrande/data/FC_01648/Unaligned_1_PF_mm1/Data/Project_yael
-
-#R libraries taken from /groups/cbdm_lab/dp133/R_libraries
-#export R_LIBS="/groups/bpf-evergrande/tools/cbdm_R_libraries/R_libraries"
-for dir in S* ; do echo $dir ; bsub -q bpf_unlimited  -n 2 -W 36:00 -R "rusage[mem=24000]" -o $dir.liRNAseq.log liRNAseq_pipeline_2_mouse.sh $dir $dir; done
