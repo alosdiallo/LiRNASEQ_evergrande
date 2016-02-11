e700039068c81f4c6a9d392b339188527b776951
ashleysn8 <ashleysn8@gmail.com>
2016-02-10 23:54:51 -0500

    Added in more comments

--- liRNAseq_newdpzpipeline/RUN.py
+++ liRNAseq_newdpzpipeline/RUN.py
@@ -8,15 +8,15 @@ from optparse import OptionParser
 
 parser = argparse.ArgumentParser(description="Run Genomes through scripts")
 
-parser.add_argument("-p", help="path of the genomes")
-parser.add_argument("-t", type=int, help="transcriptome")
-parser.add_argument("-u", help="UMI Length")
-parser.add_argument("-c", help="The path of the directory that this code directory is in *including this directory*")
-parser.add_argument("-time", help="The amount of hours the genome will take to process")
-parser.add_argument("-bf", help="The start of the barcode")
-parser.add_argument("-bl", help="The end of the barcode")
-parser.add_argument("-uf", help="The start of the UMI")
-parser.add_argument("-ul", help="The end of the UMI")
+parser.add_argument("-p", help="path of the genomes")#accepts the path parameter
+parser.add_argument("-t", type=int, help="transcriptome")#accepts the transcriptome parameter
+parser.add_argument("-u", help="UMI Length")#accepts the UMI length parameter
+parser.add_argument("-c", help="The path of the directory that this code directory is in *including this directory*")#accepts the current path parameter
+parser.add_argument("-time", help="The amount of hours the genome will take to process")#accepts the time parameter
+parser.add_argument("-bf", help="The start of the barcode")#accepts the first barcode parameter
+parser.add_argument("-bl", help="The end of the barcode")#accepts the last barcode parameter
+parser.add_argument("-uf", help="The start of the UMI")#accepts the first umi parameter
+parser.add_argument("-ul", help="The end of the UMI")#accepts the last umi parameter
 args = parser.parse_args()
 
 fileHandle = open(os.path.realpath(args.p), 'r+')#opens the file that is going to be run through
@@ -31,15 +31,7 @@ def copyFile(args.p, currentPath):
     except IOError as e:
         print('Error: %s' % e.strerror)
 
-    
-if "Human" in species or "human" in species:
-    os.system("runScript_human.sh " + args.p)#if input as human then it will run the human runScript in
-    subprocess.call(['./liRNAseq_pipeline_2_human.sh '] + args.p + " " + args.p + " " + args.u)#runs the low input RNA seqeunce script for human and passes in the args.p and UMI length
-elif "mouse" in species or "Mouse" in species:
-    os.system("runScript_mouse.sh " + args.p)#if input as mouse then it will run the mouse runScript in
-    subprocess.call(['./liRNAseq_pipeline_2_mouse.sh '] + args.p + " " + args.p + " " + args.u)#runs low input RNA sequence script for mouse and passes in the args.p and the UMI length
-else :
-    print ("Please try again")#in case there was a misspelling
+    os.system("runScript_mouse.sh " + args.p + args.time)#calls the runScript
     
 myFolders = []  #list of files in folder
         
