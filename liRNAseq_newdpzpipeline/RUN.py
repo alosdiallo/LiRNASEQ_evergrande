44b75efdbd25811f7164525c7879875362f36504
ashleysn8 <ashleysn8@gmail.com>
2016-02-10 19:21:55 -0500

    Changed both scripts to accept parameters

--- liRNAseq_newdpzpipeline/RUN.py
+++ liRNAseq_newdpzpipeline/RUN.py
@@ -3,19 +3,24 @@ import bz2
 import sys
 import shutil
 import subprocess
+import argparse
+from optparse import OptionParser
 
-species = input("What is the Species? ")#Client input of either human or mouse
-genome = input("What is the genome path? ")#Client input of the set data to be run through
-umiLength = input("What is the UMI length? ")#Client input of the UMI length
-currentPath = input("What is the current path? ")#Client input of the directory that the local directory is in
+parser = argparse.ArgumentParser(description="Run Genomes through scripts")
 
-fileHandle = open(os.path.realpath(genome), 'r+')#opens the file that is going to be run through
-genomeSize = os.path.getsize(genome)#gets the file size of the genome file
-uncompressedData = bz2.BZ2File(genome).read(genomeSize)#Decompressed the data set in .bz2 and does so until the size that was determined earlier
+parser.add_argument("-p", help="path of the genomes")
+parser.add_argument("-t", type=int, help="transcriptome")
+parser.add_argument("-u", help="UMI Length")
+parser.add_argument("-c", help="The path of the directory that this code directory is in *including this directory*")
+args = parser.parse_args()
 
-def copyFile(genome, currentPath):
+fileHandle = open(os.path.realpath(args.p), 'r+')#opens the file that is going to be run through
+genomeSize = os.path.getsize(args.p)#gets the file size of the args.p file
+uncompressedData = bz2.BZ2File(args.p).read(genomeSize)#Decompressed the data set in .bz2 and does so until the size that was determined earlier
+
+def copyFile(args.p, currentPath):
     try:
-        shutil.copy(genome, currentPath)#copies the genome to the local directory
+        shutil.copy(args.p, currentPath)#copies the args.p to the local directory
     except shutil.Error as e:
         print('Error: %s' % e)
     except IOError as e:
@@ -23,25 +28,25 @@ def copyFile(genome, currentPath):
 
     
 if "Human" in species or "human" in species:
-    os.system("runScript_human.sh " + genome)#if input as human then it will run the human runScript in
-    subprocess.call(['./liRNAseq_pipeline_2_human.sh '] + genome + " " + genome + " " + umiLength)#runs the low input RNA seqeunce script for human and passes in the genome and UMI length
+    os.system("runScript_human.sh " + args.p)#if input as human then it will run the human runScript in
+    subprocess.call(['./liRNAseq_pipeline_2_human.sh '] + args.p + " " + args.p + " " + args.u)#runs the low input RNA seqeunce script for human and passes in the args.p and UMI length
 elif "mouse" in species or "Mouse" in species:
-    os.system("runScript_mouse.sh " + genome)#if input as mouse then it will run the mouse runScript in
-    subprocess.call(['./liRNAseq_pipeline_2_mouse.sh '] + genome + " " + genome + " " + umiLength)#runs low input RNA sequence script for mouse and passes in the genome and the UMI length
+    os.system("runScript_mouse.sh " + args.p)#if input as mouse then it will run the mouse runScript in
+    subprocess.call(['./liRNAseq_pipeline_2_mouse.sh '] + args.p + " " + args.p + " " + args.u)#runs low input RNA sequence script for mouse and passes in the args.p and the UMI length
 else :
     print ("Please try again")#in case there was a misspelling
     
 myFolders = []  #list of files in folder
         
-for root, dirs, files in os.walk(genome):
+for root, dirs, files in os.walk(args.p):
     myFolders.extend(files) #add files name into list
     for d in myFolders:
         folder_name = d.rsplit('.',1)[0]#If file calls abc.txt , so takes abc
         folderNames.extend(folder_name)
-        newpath = os.path.join(genome,folder_name)   
+        newpath = os.path.join(args.p,folder_name)   
         if not os.path.exists(newpath): #check if folder exists
             os.makedirs(newpath)    #create folder based on the filenames
-        file_original_path = os.path.join(genome,d)
+        file_original_path = os.path.join(args.p,d)
         shutil.copy(file_original_path,newpath) #copy the corresponding files to the folder
 
 print ("Part I succesful!")
--- liRNAseq_newdpzpipeline/result_dir_maker.py
+++ liRNAseq_newdpzpipeline/result_dir_maker.py
@@ -2,7 +2,7 @@ import os
 import glob
 from Yael_genomes_param.py import *#imports the neccasary variables from Yael_genomes_param.py 
 
-resultPath = currentPath + "\Results"#creates the new path with the Results folder
+resultPath = args.c + "\Results"#creates the new path with the Results folder
 if not os.path.exists(result):#checks whether folder already exists
     os.makedirs("Results")#creates a folder called Results
 
