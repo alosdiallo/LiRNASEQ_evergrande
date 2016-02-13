1e4e5cd2427958c878fe120d53aced164a1be69b
ashleysn8 <ashleysn8@gmail.com>
2016-02-13 06:23:49 -0500

    Fixed a none closed file path and got rid of one parameter

--- liRNAseq_newdpzpipeline/RUN.py
+++ liRNAseq_newdpzpipeline/RUN.py
@@ -4,22 +4,22 @@ import sys
 import shutil
 import subprocess
 import argparse
-from optparse import OptionParser
 
 parser = argparse.ArgumentParser(description="Run Genomes through scripts")
 
 parser.add_argument("-p", help="path of the genomes")#accepts the path parameter
 parser.add_argument("-t", help="transcriptome")#accepts the transcriptome parameter
-parser.add_argument("-c", help="The path of the directory that this code directory is in *including this directory*")#accepts the current path parameter
 parser.add_argument("-time", type=int, help="The amount of hours the genome will take to process")#accepts the time parameter
 parser.add_argument("-bc", help="The start of the barcode")#accepts the barcode length
 parser.add_argument("-ul", help="The start of the UMI")#accepts the UMI length
 args = parser.parse_args()
 
-fileHandle = open(os.path.realpath(args.p), 'r+')#opens the file that is going to be run through
-genomeSize = os.path.getsize(args.p)#gets the file size of the args.p file
-uncompressedData = bz2.BZ2File(args.p).read(genomeSize)#Decompressed the data set in .bz2 and does so until the size that was determined earlier
+with open(os.path.realpath(args.p), 'r+') as f:#opens the file that is going to be run through
+    genomeSize = os.path.getsize(f)#gets the file size of the args.p file
+    uncompressedData = bz2.BZ2File(f).read(genomeSize)#Decompressed the data set in .bz2 and does so until the size that was determined earlier
 
+f.closed    
+    
 def copyFile(args.p, currentPath):
     try:
         shutil.copy(args.p, currentPath)#copies the args.p to the local directory
