f9852a06ed271b4aa3439bed9dfae61a0b81e398
ashleysn8 <ashleysn8@gmail.com>
2016-02-21 17:05:24 -0500

    minor fixes to test_folder.py

--- liRNAseq_newdpzpipeline/test_folder.py
+++ liRNAseq_newdpzpipeline/test_folder.py
@@ -6,23 +6,26 @@ import subprocess
 import argparse
 import glob
 import pprint
+import re
 
 parser = argparse.ArgumentParser(description="Run Genomes through scripts")
 
 parser.add_argument("-p", help="path of the experiment")#accepts the path parameter
 args = parser.parse_args()
 
+dirs = os.listdir(args.p)
 
-for s in glob.glob(args.p):
-    organized = s.rsplit('_')[2]
-    y=[]
-    y.append(os.path.join(args.p, organized)
-        pprint.pprint(locals(y))
-    for expfolders in y:
-        if not os.path.exists(expfolders)
-            os.mkdir(expfolders)
-            if y[4] == "R1"
-                shutil.move(s, os.path.join(s, os.path.basename(s)))
-            elif y[4] == "R2"
-                shutil.move(s, os.path.join(s, os.path.basename(s)))
-            else
+for file in dirs:
+    exptype = file.split('_')
+    arrexptype = []
+    arrexptype.extend(exptype[2])
+    for expfolders in arrexptype:
+        expfolderpath = os.path.join(args.p, expfolders)
+        if not os.path.exists(expfolderpath):
+            os.mkdir(expfolderpath)
+            if re.match("R1", exptype[4]):
+                shutil.move(file, os.path.join(expfolderpath, arrexptype[4]))
+            elif re.match("R2", exptype[4]):
+                shutil.move(file, os.path.join(expfolderpath, arrexptype[4]))
+            else:
+                print("Error")
