0bd88d30eda2ccac064fb08647f30ce01054e8a8
Alos Diallo <alos31@gmail.com>
2016-03-06 17:12:46 -0500

    Update to fix errors in help

--- liRNAseq_newdpzpipeline/RUN.py
+++ liRNAseq_newdpzpipeline/RUN.py
@@ -12,8 +12,8 @@ parser.add_argument("-p", help="path of the experiment")#accepts the path parame
 parser.add_argument("-g", help="path of the genome")#accepts the genome parameter
 parser.add_argument("-t", help="path of the transcriptome")#accepts the transcriptome parameter
 parser.add_argument("-time", help="The amount of hours the genome will take to process")#accepts the time parameter
-parser.add_argument("-bc", help="The start of the barcode")#accepts the barcode length
-parser.add_argument("-ul", help="The start of the UMI")#accepts the UMI length
+parser.add_argument("-bc", help="Length of barcode")#accepts the barcode length
+parser.add_argument("-ul", help="UML lenght")#accepts the UMI length
 parser.add_argument("-b", help="Name of Bed File")#What is the name of your bed file
 args = parser.parse_args()
 
