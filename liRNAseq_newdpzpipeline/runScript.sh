b33bb76d9adb818006941263d9eaf2279ac629b0
Alos Diallo <alos31@gmail.com>
2016-08-22 14:03:19 -0400

    updated Rda info
    updated Rda info

--- liRNAseq_newdpzpipeline/runScript.sh
+++ liRNAseq_newdpzpipeline/runScript.sh
@@ -4,7 +4,9 @@
 
 #R libraries taken from /groups/cbdm_lab/dp133/R_libraries
 export R_LIBS="/groups/cbdm_lab/dp133/R_libraries"
+export R_LIBS="/groups/immdiv-bioinfo/evergrande/yael/liRNAseq_newdpzpipeline"
 #editing by Alos
+module load dev/openmpi-1.8.6
 
 
 expdir=$1
@@ -14,10 +16,14 @@ tran=$4
 umi=$5
 bc=$6
 bedFile=$7
-dirCurrent=$8
+rDApath=$8
+dirCurrent=$9
+
+
 
 cd $expdir
 
 
-for dir in S*; do echo $dir ; bsub -q mcore  -n 2 -W $time -R "rusage[mem=24000]" -o $dir.liRNAseq.log $dirCurrent/liRNAseq_pipeline_2.sh $dir $dir $genome $tran $umi $bc $dirCurrent $expdir $bedFile; done
-#for dir in S* ; do echo $dir ; bsub -q mcore  -n 2 -W 36:00 -R "rusage[mem=24000]" -o $dir.liRNAseq.log /home/yj88/immdiv_bio/evergrande/yael/liRNAseq_newdpzpipeline/liRNAseq_pipeline_2_mouse.sh $dir $dir; done
+
+for dir in S*; do echo $dir ; bsub -q priority -n 4 -W 400:00 -R "rusage[mem=7000]" -o $dir.liRNAseq.log $dirCurrent/liRNAseq_pipeline_2.sh $dir $dir $genome $tran $umi $bc $dirCurrent $expdir $bedFile $rDApath; done
+
